80cf1b696a8c56cdffd1778c483338b1b2bae8ea
kitagawa <kitagawa>
2010-05-10 09:50:07 +0000

    Fix SimpleForwardToFrontend() so that it reset select_in_transaction flag and execute_select flag when bind error occurred while executing SELECT.

--- pool_process_query.c
+++ pool_process_query.c
@@ -1,6 +1,6 @@
 /* -*-pgsql-c-*- */
 /*
- * $Header: /cvsroot/pgpool/pgpool-II/pool_process_query.c,v 1.141.2.38 2010/05/10 09:49:18 kitagawa Exp $
+ * $Header: /cvsroot/pgpool/pgpool-II/pool_process_query.c,v 1.141.2.39 2010/05/10 09:50:07 kitagawa Exp $
  *
  * pgpool: a language independent connection pool server for PostgreSQL
  * written by Tatsuo Ishii
@@ -1656,7 +1656,11 @@ POOL_STATUS SimpleForwardToFrontend(char kind, POOL_CONNECTION *frontend, POOL_C
 			free(pending_prepared_portal);
 		}
 	}
-	else if (kind == 'C' && select_in_transaction)
+	/* 
+	 * The response of Execute command will be EmptyQueryResponse(I),
+	 * if Bind error occurs.
+	 */
+	else if ((kind == 'C' || kind == 'I') && select_in_transaction)
 	{
 		select_in_transaction = 0;
 		execute_select = 0;
