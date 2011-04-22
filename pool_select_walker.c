7232cf4d0ed67af800fe431febd3c19de1e8f12a
kitagawa <kitagawa>
2011-04-22 11:40:37 +0000

    Increase relcache size from 32 to 128. This has the effect of reducing "unnamed prepared statement does not exist" error.
    Bug report from Joseph Jacob. See:
    Subject: Fwd: Test java program
    Date: Fri, 15 Apr 2011 12:16:04 -0700

--- pool_select_walker.c
+++ pool_select_walker.c
@@ -1,6 +1,6 @@
 /* -*-pgsql-c-*- */
 /*
- * $Header: /cvsroot/pgpool/pgpool-II/pool_select_walker.c,v 1.8 2011/01/19 02:52:59 t-ishii Exp $
+ * $Header: /cvsroot/pgpool/pgpool-II/pool_select_walker.c,v 1.9 2011/04/22 11:40:37 kitagawa Exp $
  *
  * pgpool: a language independent connection pool server for PostgreSQL
  * written by Tatsuo Ishii
@@ -296,7 +296,7 @@ static bool is_system_catalog(char *table_name)
 			query = ISBELONGTOPGCATALOGQUERY;
 		}
 
-		hasreliscatalog_cache = pool_create_relcache(32, query,
+		hasreliscatalog_cache = pool_create_relcache(128, query,
 										int_register_func, int_unregister_func,
 										false);
 		if (hasreliscatalog_cache == NULL)
@@ -315,7 +315,7 @@ static bool is_system_catalog(char *table_name)
 		 */
 		if (!relcache)
 		{
-			relcache = pool_create_relcache(32, ISBELONGTOPGCATALOGQUERY,
+			relcache = pool_create_relcache(128, ISBELONGTOPGCATALOGQUERY,
 											int_register_func, int_unregister_func,
 											true);
 			if (relcache == NULL)
@@ -384,7 +384,7 @@ static bool is_temp_table(char *table_name)
 	 */
 	if (!hasrelistemp_cache)
 	{
-		hasrelistemp_cache = pool_create_relcache(32, HASRELITEMPPQUERY,
+		hasrelistemp_cache = pool_create_relcache(128, HASRELITEMPPQUERY,
 										int_register_func, int_unregister_func,
 										false);
 		if (hasrelistemp_cache == NULL)
@@ -405,7 +405,7 @@ static bool is_temp_table(char *table_name)
 	 */
 	if (!relcache)
 	{
-		relcache = pool_create_relcache(32, query,
+		relcache = pool_create_relcache(128, query,
 										int_register_func, int_unregister_func,
 										true);
 		if (relcache == NULL)
